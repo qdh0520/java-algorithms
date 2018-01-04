582cf5c003ad8f42c59bd39012bfbf28ad684cf0
qdh0520 <qiudanhui0520@gmail.com>
2018-01-04 21:04:17 +0800

    Myqueue

---
+++ problems/src/queue/Test.java
@@ -0,0 +1,24 @@
+package queue;
+
+// Driver class
+public class Test
+{
+    public static void main(String[] args)
+    {
+        MyQueue queue = new MyQueue(1000);
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+
+        System.out.println(queue.dequeue() +
+                " dequeued from queue\n");
+
+        System.out.println("Front item is " +
+                queue.front());
+
+        System.out.println("Rear item is " +
+                queue.rear());
+    }
+}
